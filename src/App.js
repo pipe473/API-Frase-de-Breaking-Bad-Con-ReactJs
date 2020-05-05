46b9809e7b14e534390c589521bfc733cf0d1599
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-05 19:30:14 +0200

    Añadiendo el state para guardar las frases

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
 const Contenedor = styled.div`
@@ -19,10 +19,14 @@ const Boton = styled.button`
 `;
 
 function App() {
+
+  // State de frases
+  const [frase, guardarFrase ] = useState({});
+
 	const consultarAPI = async () => {
 		const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
     const frase = await api.json()
-    console.log(frase[0]);        
+   guardarFrase(frase[0]);        
 	}
 
 	return (
