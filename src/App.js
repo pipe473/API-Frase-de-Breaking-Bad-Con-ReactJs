1c29b86e2e3ef0dea0337515c47511509daeaeb2
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-05 23:22:33 +0200

    Finalizando proyecto, añadiendo los ultimos estilos al boton

--- src/App.js
+++ src/App.js
@@ -16,7 +16,13 @@ const Boton = styled.button`
 	margin-top: 3rem;
 	padding: 1rem 3rem;
 	font-size: 2rem;
-	border: 2px solid black;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
 `;
 
 function App() {
