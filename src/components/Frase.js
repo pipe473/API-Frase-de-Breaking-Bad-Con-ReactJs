25eba34d9c893b28f2525ecbe9190b39fc924b09
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-05 23:12:18 +0200

    Añadiendo estilos con Sass

--- src/components/Frase.js
+++ src/components/Frase.js
@@ -10,6 +10,31 @@ const ContenedorFrase = styled.div`
     @media (min-width: 992px){
         margin-top: 10rem;
     }
+
+    h1 {
+        font-family: Arial, Helvetica, sans-serif;
+        text-align: center;
+        position: relative;
+        padding-left: 4rem;
+
+        &::before {
+            content: open-quote;
+            font-size: 10rem;
+            color: black;
+            position: absolute;
+            left: -1rem;
+            top: -2rem;
+        }
+    }
+
+    p {
+        font-family: Verdana, Geneva, Tahoma, sans-serif;
+        font-size: 1.4rem;
+        font-weight: bold;
+        text-align: right;
+        color: #666;
+        margin-top: 2rem;
+    }
 `;
 
 const Frase = ({ frase }) => {
